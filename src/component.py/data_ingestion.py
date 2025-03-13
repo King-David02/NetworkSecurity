25534bdfcb93cc413ffd2cccf8f14c3353070f84
King-David <ajanakingdavid@outlook.com>
2025-03-13 13:27:41 +0100

    Data ingestion

---
+++ src/component.py/data_ingestion.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import pymongo
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+from src.entity.artifacts_entity import DataIngestionArtifact
+from src.entity.config_entity import DataIngestionConfig
+from sklearn. model_selection import train_test_split
+from src.exception.exception import CustomException
+from src.logging.logger import logging
+
+
+load_dotenv()
+MONGO_DB_URL = os.getenv('MONGO_DB_URL')
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+        self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+
+    def retrieve_data_from_mongo(self):
+        try:
+            collection = self.mongo_client[self.config.database_name][self.config.collection_name]
+            df = pd.DataFrame(list(collection.find())).df.drop(columns=["_id"], errors='ignore')
+            df.replace({"na": pd.NA}, inplace=True)
+            return df
+        
+        except Exception as e:
+            raise CustomException(e, sys.exc_info())
+    
+    def save_to_dataframe(self, df, file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        df.to_csv(file_path, index=False)
+
+
+    def initiate_data_ingestion(self):
+        try:
+            logging.info("Data ingestion started")
+            df = self.retrieve_data_from_mongo()
+            logging.info("Datareceived from Mongodb")
+            self.save_to_dataframe(df)
+            logging.info("Raw file Saved")
+
+            train_size, test_size = train_test_split(df, test_size=self.config.train_test_split_ratio, random_state=42)
+            logging.info("train test split done")
+            self.save_to_dataframe(train_size, self.config.train_file_path)
+            logging.info("train data saved")
+            self.save_to_dataframe(test_size, self.config.test_file_path)
+            logging.info("test data saved")
+            artifacts = DataIngestionArtifact(trained_file_path=self.config.train_file_path, test_file_path=self.config.test_file_path)
+            logging.info("Data ingestion done")
+            return artifacts
+        
+        except Exception as e:
+            raise CustomException(e, sys.exc_info())
+    
---
+++ src/entity/config_entity.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import os
+from src.constant import training_pipeline as tp
+
+class TrainingPipelineConfig:
+    def __init__(self, timestamp= datetime().now()):
+        timestamp = timestamp.strftime("%d_%m_%Y_%H-%M-%S")
+        self.pipeline_name = tp.PIPELINE_NAME
+        self.artifacts_dir = os.path.join(tp.ARTIFACT_DIR, timestamp)
+        self.model_dir=os.path.join("final_model")
+
+    
+class DataIngestionConfig:
+    def __init__(self, ingestion_config: TrainingPipelineConfig):
+        self.ingestion_base_dir = os.path.join(ingestion_config.artifacts_dir, tp.DATA_INGESTION_DIR_NAME)
+        self.feature_store = os.path.join(self.ingestion_base_dir, tp.DATA_INGESTION_FEATURE_STORE_DIR, tp.FILE_NAME)
+        self.ingested_dir = os.path.join(self.ingestion_base_dir, tp.DATA_INGESTION_INGESTED_DIR)
+        self.train_file_path = os.path.join(self.ingested_dir, tp.TRAIN_FILE_NAME)
+        self.test_file_path = os.path.join(self.ingested_dir, tp.TEST_FILE_NAME)
+        self.train_test_split_ratio = tp.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+        self.collection_name = tp.DATA_INGESTION_COLLECTION_NAME
+        self.database_name = tp.DATA_INGESTION_DATABASE_NAME
