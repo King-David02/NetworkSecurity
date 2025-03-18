c6bff4ddf4f4ee9b67ad3505061456e7f5afcb5a
King-David <ajanakingdavid@outlook.com>
2025-03-18 12:51:50 +0100

    Dagshub, MLflow

--- src/component/model_trainer.py
+++ src/component/model_trainer.py
@@ -15,7 +15,10 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
+import dagshub
+import mlflow
 
+dagshub.init(repo_owner='King-David02', repo_name='NetworkSecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig, artifacts: DataTransformationArtifact):
@@ -44,24 +47,34 @@ class ModelTrainer:
             })
         }
             
+            mlflow.autolog()
+            with mlflow.start_run():
 
-            train_arr = load_numpy_array_data(self.artifacts.transformed_train_file_path)
-            test_arr = load_numpy_array_data(self.artifacts.transformed_test_file_path)
+                train_arr = load_numpy_array_data(self.artifacts.transformed_train_file_path)
+                test_arr = load_numpy_array_data(self.artifacts.transformed_test_file_path)
 
-            X_train, y_train = train_arr[:,:-1], train_arr[:, -1]
-            X_test, y_test = test_arr[:,:-1], test_arr[:, -1]
+                X_train, y_train = train_arr[:,:-1], train_arr[:, -1]
+                X_test, y_test = test_arr[:,:-1], test_arr[:, -1]
 
-            logging.info("Model Training Begins")
-            best_model, R2 = evaluate_model(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, model_params=models)
-            logging.info("Model Training complete")
-            logging.info(f"The best_model is {best_model} with R2 score of {R2}")
-            save_object(self.config.saved_model_path, best_model)
-            logging.info("Best Model Saved")
-            y_train_pred = best_model.predict(X_train)
-            y_test_pred = best_model.predict(X_test)
+                logging.info("Model Training Begins")
+                best_model, R2 = evaluate_model(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, model_params=models)
+                logging.info("Model Training complete")
+                logging.info(f"The best_model is {best_model} with R2 score of {R2}")
+                logging.info("Best Model Saved")
+                y_train_pred = best_model.predict(X_train)
+                y_test_pred = best_model.predict(X_test)
 
-            train_metrics = get_classification_score(y_train, y_train_pred)
-            test_metrics = get_classification_score(y_test, y_test_pred)
+                train_metrics = get_classification_score(y_train, y_train_pred)
+                test_metrics = get_classification_score(y_test, y_test_pred)
+
+                mlflow.log_metric("train_f1_score", train_metrics.f1_score)
+                mlflow.log_metric("train_precision", train_metrics.precision_score)
+                mlflow.log_metric("train_recall", train_metrics.recall_score)
+                mlflow.log_metric("test_f1_score", test_metrics.f1_score)
+                mlflow.log_metric("test_precision", test_metrics.precision_score)
+                mlflow.log_metric("test_recall", test_metrics.recall_score)
+                save_object(self.config.saved_model_path, best_model)
+
             artifacts = ModelTrainerArtifact(trained_model_file_path=self.config.saved_model_path, train_metric_artifact=train_metrics,test_metric_artifact=test_metrics)
             return artifacts
         
